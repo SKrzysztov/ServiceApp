3f950d13d633cec4870ee056cd45edb1f82a5d99
SKrzysztov <122411690+SKrzysztov@users.noreply.github.com>
2023-12-12 21:55:37 +0100

    Database Config
    Database Config
    
    Database Config

--- src/main/java/com/example/project/customservice/controller/CustomServiceController.java
+++ src/main/java/com/example/project/customservice/controller/CustomServiceController.java
@@ -1,4 +1,43 @@
 package com.example.project.customservice.controller;
 
+import com.example.project.customservice.api.CustomServiceRequest;
+import com.example.project.customservice.domain.CustomService;
+import com.example.project.customservice.service.CustomServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/services")
 public class CustomServiceController {
+
+    @Autowired
+    private CustomServiceService customServiceService;
+
+    @GetMapping
+    public List<CustomService> getAllServices() {
+        return customServiceService.getAllServices();
+    }
+
+    @GetMapping("/{serviceId}")
+    public CustomService getServiceById(@PathVariable Long serviceId) {
+        return customServiceService.getServiceById(serviceId)
+                .orElseThrow(() -> new RuntimeException("Serwis o podanym ID nie istnieje"));
+    }
+
+    @PostMapping("/create/{userId}")
+    public CustomService createService(@PathVariable Long userId, @RequestBody CustomServiceRequest serviceRequest) {
+        return customServiceService.createService(userId, serviceRequest);
+    }
+
+    @PutMapping("/{serviceId}")
+    public CustomService updateService(@PathVariable Long serviceId, @RequestBody CustomServiceRequest serviceRequest) {
+        return customServiceService.updateService(serviceId, serviceRequest);
+    }
+
+    @DeleteMapping("/{serviceId}")
+    public void deleteService(@PathVariable Long serviceId) {
+        customServiceService.deleteService(serviceId);
+    }
 }
--- src/main/java/com/example/project/customservice/domain/CustomService.java
+++ src/main/java/com/example/project/customservice/domain/CustomService.java
@@ -1,4 +1,28 @@
 package com.example.project.customservice.domain;
 
+import com.example.project.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String serviceName;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "user_id")
+    private User user;
 }
